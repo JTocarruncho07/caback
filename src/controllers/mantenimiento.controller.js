c3664d878cce0936d443465ab2756dfbfe250049
Juan Pablo Tocarruncho <jtocarruncho07@gmail.com>
2025-04-07 10:11:39 -0500

    Update mantenimiento.controller.js

--- src/controllers/mantenimiento.controller.js
+++ src/controllers/mantenimiento.controller.js
@@ -48,6 +48,7 @@ const eliminarMantenimiento = catchError(async (req, res) => {
       res.status(500).json({ error: error.message });
     }
   });
+  
 module.exports = {
     crearMantenimiento,
     obtenerMantenimientosPorActivo,
